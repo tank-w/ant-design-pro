473e5549ad27e1d1e789fb305246b8d38e255cb2
wxq <845169275@qq.com>
2019-11-06 09:00:11 +0800

    update

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+export default {
+  navTheme: 'dark',
+  primaryColor: '#ff0000',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '测试后台',
+  pwa: false,
+  iconfontUrl: '',
+};
